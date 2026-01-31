1c008338031fec0dd72f161ebb6f0f0d9ebd0d82
bitowl <git@bitowl.de>
2026-01-31 09:52:26 +0100

    Add tilemap pathfinding to simple enemy

--- scenes/enemies/simple_enemy.gd
+++ scenes/enemies/simple_enemy.gd
@@ -1,13 +1,15 @@
 extends CharacterBody2D
 
 @export var speed: float = 50
+@onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 
 var target: Player
 var is_player_detected: bool = false
 
 func _physics_process(_delta: float) -> void:
 	if is_player_detected:
-		var direction_to_player = (target.position - position).normalized()
+		var next_target = navigation_agent.get_next_path_position()
+		var direction_to_player = (next_target - position).normalized()
 		velocity = direction_to_player * speed
 		move_and_slide()
 
@@ -18,3 +20,11 @@ func _on_detection_area_body_entered(_body: Node2D) -> void:
 
 func _on_detection_area_body_exited(_body: Node2D) -> void:
 	is_player_detected = false
+
+
+func _on_pathfinding_timer_timeout() -> void:
+	navigation_agent.target_position = target.position
+
+
+func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
+	pass # Replace with function body.
