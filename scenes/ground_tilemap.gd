634c7850d396092712709c45bbc43c8096db81ed
bitowl <git@bitowl.de>
2026-01-31 22:33:03 +0100

    Fix error in tilemaps

--- scenes/ground_tilemap.gd
+++ scenes/ground_tilemap.gd
@@ -5,6 +5,8 @@ extends TileMapLayer
 # Delete navigation for tiles which have a tile in the above fence tilemap.
 
 func _use_tile_data_runtime_update(coords: Vector2i) -> bool:
+	if not wall_tilemap:
+		return false
 	return coords in wall_tilemap.get_used_cells_by_id(0)
 	
 func _tile_data_runtime_update(coords: Vector2i, tile_data: TileData) -> void:
