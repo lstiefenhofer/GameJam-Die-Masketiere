1dbd638c24e3a7307eca56e052bed427f5c8abcc
Robin <robin@hohnsbeen.de>
2026-01-31 22:36:38 +0100

    Enemy: Target reassignment, better timer management, pushback, pushback interrupts attacks, cooldown timers at finish attack, navigation timeout, utility functions, iniitial overlap to check if player is nearby

--- scenes/enemies/enemy_base.gd
+++ scenes/enemies/enemy_base.gd
@@ -2,46 +2,82 @@ extends CharacterBody2D
 class_name EnemyBase
 
 @export var speed: float = 50
+@export var attack_cooldown_min: float = 0.5
+@export var attack_cooldown_max: float = 1.5
+@export var forget_radius: float = 200.0
 var _default_max_speed = 100
 var _default_speed = 50
 @export var health: float = 3:
 	set(value):
 		health = value
 		if health <= 0:
-			die()
+			_die()
 @export var damage: float = 1
+
 
 @onready var navigation_agent: NavigationAgent2D = $NavigationAgent2D
 @onready var enemy_placeholder: Sprite2D = $EnemyPlaceholder
 @onready var enemy_spritesheet: AnimatedSpriteFlash = $EnemySpritesheet
 @onready var sprite_flash: SpriteFlash = $EnemyPlaceholder
-@onready var attack_timer: Timer = $AttackTimer
 @onready var attack_sprite: Sprite2D = $AttackSprite
+@onready var nearby_shape: CollisionShape2D = $DetectionArea/CollisionShape2D
 
-var target: Player
-var is_player_detected: bool = false
+var target: Node2D
 var is_player_in_attack_range: bool = false
 
 var can_move = true # For movement controls in child scripts.
 var _is_in_override_navigation = false
 var _is_attacking = false
+var _current_pushback_intensity = 0.0
+var _current_pushback_velocity = Vector2.ZERO
+var _time_spend_on_current_navigation = 0.0
+var _max_time_spending_in_navigation = 5.0
+var _first_overlap_check = true # Check on spawn, if player is nearby
+
+var active_timers : Array[Timer]
 
 signal target_reached()
 
 func _ready() -> void:
-	target = Player.player
+	#target = Player.player
 	
 	navigation_agent.navigation_finished.connect(Callable(self, "_move_to_finished"))
 	_default_max_speed = navigation_agent.max_speed
 	_default_speed = speed
 
 
-func get_target() -> Player:
-	return Player.player
+func create_local_timer(duration) -> Timer:
+	if !is_inside_tree():
+		return null
+	
+	var timer = Timer.new()
+	add_child(timer)
+	timer.start(duration)
+	active_timers.append(timer)
+	return timer
+
+
+func get_target() -> Node2D:
+	return target
+	#return Player.player
 
 
 func _process(delta: float) -> void:
 	choose_attack()
+	
+	if _current_pushback_intensity >= 0:
+		_current_pushback_intensity -= 0.5 * delta
+	else:
+		_current_pushback_intensity = 0
+	
+	# Unassign target when it gets too far away
+	if target and target.global_position.distance_squared_to(global_position) > forget_radius * forget_radius:
+		target = null
+		
+	if _is_in_override_navigation:
+		_time_spend_on_current_navigation += delta
+		if _time_spend_on_current_navigation > _max_time_spending_in_navigation:
+			stop_movement_override()
 
 
 func choose_attack():
@@ -59,9 +95,9 @@ func choose_attack():
 		
 
 func _physics_process(_delta: float) -> void:
-	if is_player_detected and can_move:
+	if get_target() and can_move:
 		var next_target = navigation_agent.get_next_path_position()
-		var direction_to_player = (next_target - position).normalized()
+		var direction_to_player = (next_target - global_position).normalized()
 		var velocity_to_next_target = direction_to_player * speed
 		if navigation_agent.avoidance_enabled:
 			# Set the velocity on the navigation agent and then get the safe velocity after avoidance in _on_navigation_agent_2d_velocity_computed.
@@ -74,30 +110,42 @@ func _physics_process(_delta: float) -> void:
 		if enemy_spritesheet:
 			enemy_spritesheet.flip_h = velocity.x < 0
 		
+		velocity += _current_pushback_intensity * _current_pushback_velocity
 		move_and_slide()
 		
-	else:
+	elif _current_pushback_intensity == 0:
 		velocity = Vector2.ZERO
+		
+	elif _current_pushback_intensity > 0:
+		velocity = _current_pushback_intensity * _current_pushback_velocity
+		move_and_slide()
+		
+	#print("Intensity %d, Velocity %v" % [_current_pushback_intensity, _current_pushback_velocity])
+
+	# Only check once if there is a player already inside our physics shape
+	if !target and _first_overlap_check:
+		var params = PhysicsShapeQueryParameters2D.new()
+		params.shape_rid = nearby_shape.shape.get_rid()
+		var space_state = get_world_2d().direct_space_state
+		var intersections : Array[Dictionary] = space_state.intersect_shape(params, 6)
+		for intersection in intersections:
+			var collider = intersection["collider"]
+			if collider and collider is Player:
+				target = collider
+				break
+		_first_overlap_check = false
 
 
 func set_sprite_direction(look_at_location : Vector2):
 	if enemy_placeholder:
-		enemy_placeholder.flip_h = look_at_location.x < position.x
+		enemy_placeholder.flip_h = look_at_location.x < global_position.x
 	if enemy_spritesheet:
-		enemy_spritesheet.flip_h = look_at_location.x < position.x
-
-
-func _on_detection_area_body_entered(_body: Node2D) -> void:
-	is_player_detected = true
-
-
-func _on_detection_area_body_exited(_body: Node2D) -> void:
-	is_player_detected = false
+		enemy_spritesheet.flip_h = look_at_location.x < global_position.x
 
 
 func _on_pathfinding_timer_timeout() -> void:
 	if is_instance_valid(get_target()) and !_is_in_override_navigation:
-		navigation_agent.target_position = get_target().position
+		navigation_agent.target_position = get_target().global_position
 
 
 func set_movement_speed_multiplier(in_multiplier : float):
@@ -107,6 +155,7 @@ func set_movement_speed_multiplier(in_multiplier : float):
 # Call in child scripts
 func move_to(new_position : Vector2) -> bool:
 	can_move = true
+	_time_spend_on_current_navigation = 0.0
 	_is_in_override_navigation = true
 	navigation_agent.target_position = new_position
 	if !navigation_agent.is_target_reachable():
@@ -133,35 +182,59 @@ func _on_navigation_agent_2d_velocity_computed(safe_velocity: Vector2) -> void:
 	velocity = safe_velocity
 	
 
-func take_damage(incoming_damage: float) -> void:
+func take_damage(instigator : CharacterBody2D, incoming_damage: float, pushback_velocity : Vector2 = Vector2.ZERO) -> void:
 	health -= incoming_damage
 	sprite_flash.flash(0.1, 0.2)
+	if pushback_velocity.length_squared() > 0:
+		_current_pushback_intensity = 1.0
+		_current_pushback_velocity = pushback_velocity
+		interrupt_attack()
 
 
-func die() -> void:
-	queue_free()
+func get_target_direction() -> Vector2:
+	if !get_target():
+		return Vector2(1.0, 0.0)
+		
+	var direction = (get_target().global_position - global_position).normalized()
+	return direction
+
+
+func get_position_in_arc_before_target(position_arc_degrees = 90.0, position_arc_distance = 80.0):
+	if !get_target():
+		return Vector2.UP
+	
+	var arc_degrees_rad = deg_to_rad(position_arc_degrees)
+	return get_target().global_position + (-get_target_direction()).rotated(randf_range(-arc_degrees_rad, arc_degrees_rad)) * position_arc_distance
 
 
 # Implement in child
 func start_attack() -> bool:
 	if is_player_in_attack_range:
 		if randi_range(0, 1) == 1:
-			charge_attack()
+			charge_attack(damage)
 		else:
-			default_attack()
+			default_attack(damage)
 		return true
 		
 	return false
 	
 
-func default_attack():
+func default_attack(in_damage : int):
 	can_move = false
 	
-	attack_timer.start()
+	var timer_duration = 1.0
+	var timer = create_local_timer(timer_duration)
+	if !timer:
+		finish_attack()
+		return
 
-	while(attack_timer.time_left > 0.1):
+	while(timer.time_left > 0.1):
+		if(!get_target()):
+			finish_attack()
+			break
+		
 		# Attack sprite to hint where the enemy is attacking.
-		var progress = 1.0 - attack_timer.time_left / attack_timer.wait_time
+		var progress = 1.0 - timer.time_left / timer_duration
 		attack_sprite.modulate.a = progress * 0.5
 		# We need to cancel the loop if we are not inside the tree, else we have an infinite loop.
 		if is_inside_tree(): 
@@ -171,46 +244,42 @@ func default_attack():
 	
 	attack_sprite.modulate.a = 0.0
 	can_move = true
-	hit_player_in_range()
-	await get_tree().create_timer(2.0).timeout
+	hit_player_in_range(in_damage)
 	finish_attack()
 
 
-func get_target_direction() -> Vector2:
-	if !get_target():
-		return Vector2(1.0, 0.0)
-		
-	var direction = (get_target().position - position).normalized()
-	return direction
-
-
-func charge_attack(position_arc_degrees = 90.0, position_arc_distance = 80.0, charge_speed_multiplier = 3.0, charge_audio_player : AudioStreamPlayer2D = null):
+func charge_attack(in_damage : int, position_arc_degrees = 90.0, position_arc_distance = 80.0, charge_speed_multiplier = 3.0, charge_audio_player : AudioStreamPlayer2D = null):
 	if(!get_target()):
 		finish_attack()
 	
-	var arc_degrees_rad = deg_to_rad(position_arc_degrees)
 	# Move to random location on a half circle between player and enemy which is 80 pixels away from player
-	if !move_to(get_target().position + (-get_target_direction()).rotated(randf_range(-arc_degrees_rad, arc_degrees_rad)) * position_arc_distance):
-		finish_attack() # Not reachable
+	if !move_to(get_position_in_arc_before_target(position_arc_degrees, position_arc_distance)):
+		interrupt_attack() # Not reachable
 	
 	await target_reached
 	can_move = false
-	await get_tree().create_timer(0.5).timeout # Cooldown before charge
+	var timer = create_local_timer(0.5)
+	if timer:
+		await timer.timeout # Cooldown before charge
 	
 	if(!get_target()):
-		finish_attack()
+		interrupt_attack()
 	
-	set_sprite_direction(get_target().position) # Just flip sprites towards player
+	set_sprite_direction(get_target().global_position) # Just flip sprites towards player
 	
 	set_movement_speed_multiplier(charge_speed_multiplier)
-	if !move_to(get_target().position + get_target_direction() * 30.0):
-		finish_attack() # Not reachable
+	if !move_to(get_target().global_position + get_target_direction() * 30.0):
+		interrupt_attack() # Not reachable
 	
 	if charge_audio_player:
 		charge_audio_player.play()
 	
 	while(_is_in_override_navigation):
-		if hit_player_in_range():
+		if(!get_target()):
+			interrupt_attack()
+			break
+			
+		if hit_player_in_range(in_damage):
 			stop_movement_override()
 			break
 		# We need to cancel the loop if we are not inside the tree, else we have an infinite loop.
@@ -220,15 +289,65 @@ func charge_attack(position_arc_degrees = 90.0, position_arc_distance = 80.0, ch
 			return
 	
 	can_move = false
-	await get_tree().create_timer(1.0).timeout # Cooldown for next attack
 	finish_attack()
 
 
 func finish_attack():
-	_is_attacking = false
 	can_move = true
 	attack_sprite.modulate.a = 0.0
 	set_movement_speed_multiplier(1.0)
+	
+	# Cooldown for next attack
+	var timer = create_local_timer(randf_range(attack_cooldown_min, attack_cooldown_max))
+	if timer:
+		await timer.timeout 
+	
+	_is_attacking = false
+	_clear_timers()
+
+
+func interrupt_attack():
+	if !_is_attacking:
+		return
+		
+	_reset_attack_state()
+	_clear_timers()
+	
+	var timer = create_local_timer(0.2)
+	if timer:
+		await timer.timeout 
+		
+	_is_attacking = false
+
+
+func _reset_attack_state():
+	can_move = true
+	attack_sprite.modulate.a = 0.0
+	_is_in_override_navigation = false
+	set_movement_speed_multiplier(1.0)
+
+
+func _clear_timers():
+	for timer in active_timers:
+		if timer:
+			timer.stop()
+			
+	active_timers.clear()
+
+
+func _on_detection_area_body_entered(_body: Node2D) -> void:
+	on_target_assignment(_body)
+
+
+func on_target_assignment(_body: Node2D) -> void:
+	if _body is Player and !target:
+		#is_player_detected = true
+		target = _body
+
+
+func _on_detection_area_body_exited(_body: Node2D) -> void:
+	pass # Use forget range in process function
+	#is_player_detected = false
 
 
 func _on_attack_area_body_entered(_body: Node2D) -> void:
@@ -239,9 +358,14 @@ func _on_attack_area_body_exited(_body: Node2D) -> void:
 	is_player_in_attack_range = false
 
 
-func hit_player_in_range() -> bool:
+func hit_player_in_range(in_damage : int) -> bool:
 	if is_player_in_attack_range and is_instance_valid(get_target()):
-		get_target().take_damage(damage)
+		get_target().take_damage(self, in_damage)
 		return true
 		
 	return false
+	
+
+func _die() -> void:
+	_clear_timers()
+	queue_free()
