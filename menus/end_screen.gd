07e06dcb3c4ac3cce9c3bb00b3d1811dd6a6de86
Philipp Wendel <philipp@wendel.io>
2026-02-01 14:44:09 +0100

    End screen with credits

---
+++ menus/end_screen.gd
@@ -0,0 +1,16 @@
+extends Control
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	TransitionOverlay.fade_out(2)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+func _on_quit_pressed() -> void:
+	Globals.quit()
+
+func _on_main_menu_pressed() -> void:
+	Globals.goto_main_menu()
