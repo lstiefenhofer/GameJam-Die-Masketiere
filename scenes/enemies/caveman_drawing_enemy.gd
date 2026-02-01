cd0c179d02eb3a0128bded046ac2e2d0a9051a3b
Robin <robin@hohnsbeen.de>
2026-02-01 14:12:56 +0100

    Cavemen: fix allow attacking when not in melee range

--- scenes/enemies/caveman_drawing_enemy.gd
+++ scenes/enemies/caveman_drawing_enemy.gd
@@ -3,23 +3,27 @@ extends EnemyBase
 @export var spear_scene : PackedScene 
 
 func start_attack() -> bool:
-	if is_player_in_attack_range:
-		var attack_number = 2 #randi_range(0, 1) 
-		if attack_number == 1:
-			charge_attack(damage)
-		elif attack_number == 2:
-			spear_throw(damage)
-		return true
+	#if is_player_in_attack_range:
+	var attack_number = 2 #randi_range(0, 1) 
+	if attack_number == 1:
+		charge_attack(damage)
+	elif attack_number == 2:
+		spear_throw(damage)
+	return true
 		
 	return false
 	
 	
 func spear_throw(in_damage : int):
-	if !move_to(get_position_in_arc_before_target(25.0, 120.0)):
+	debug_log("Started spear attack")
+	current_attack_name = "Spear attack"
+	_is_attacking = true
+	if !move_to(get_position_in_arc_before_target(25.0, 70.0)):
 		interrupt_attack()
 		return
 	
 	await target_reached
+	debug_log("Proceed")
 	can_move = false	
 	enemy_spritesheet.play("Attack")
 	var new_spear : Node2D = spear_scene.instantiate()
