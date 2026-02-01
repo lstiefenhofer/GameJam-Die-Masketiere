c8fe7e6f5fea4164752cdcea328c32cfc822242a
Leonie Stiefenhofer <leonie.stiefenhofer@student.uni-tuebingen.de>
2026-02-01 11:39:22 +0100

    refined flicker for lamps

--- scenes/objects/lamp.gd
+++ scenes/objects/lamp.gd
@@ -5,15 +5,20 @@ var counter = 0
 func _ready() -> void:
 		$AnimatedSprite2D.play("flicker")
 
-func _physics_process(delta):
+func _physics_process(_delta):
 	pulsate()
 	
-	 
 func pulsate() -> void:
-	$PointLight2D.energy = my_sinus(counter)
+	$PointLight2D.energy = pulsating_energy_value(counter)
 	counter += 0.1
 	
-func my_sinus(x : float) -> float:
-	return  0.5 * sin(x) + 0.5
+func pulsating_energy_value(x : float) -> float:
+	#creates random flicker
+	var flicker = (randi() % 3) * 0.1
+	#creates sinus-shaped pulse
+	var pulse =  0.5 * abs(sin(0.4 * x)) + 0.7
+	var result = max(pulse + flicker, 0.3)
+	return result 
+
 	
 	
